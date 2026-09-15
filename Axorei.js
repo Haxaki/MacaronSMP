d1155335ab75f22cf30b93615e54475d35c3eb30
Haxaki_Hamane <nguyenngocdanthanh7a2@gmail.com>
2026-09-15 23:08:50 +0700

    Add files via upload

---
+++ Axorei.js
@@ -0,0 +1,37 @@
+/* ========================================
+   MACARON SMP — AXOREI PROFILE
+======================================== */
+
+const canvas = document.getElementById("skin_container");
+
+console.log("skinview3d:", typeof skinview3d);
+console.log("canvas:", canvas);
+
+const skinViewer = new skinview3d.SkinViewer({
+    canvas: canvas,
+    width: 400,
+    height: 500,
+    skin: "./Axorei.png"
+});
+
+
+/* ========================================
+   SKIN POSITION
+======================================== */
+
+skinViewer.playerObject.position.y = -0.5;
+
+
+/* ========================================
+   AUTO ROTATION
+======================================== */
+
+skinViewer.autoRotate = true;
+skinViewer.autoRotateSpeed = 0.5;
+
+
+/* ========================================
+   ZOOM
+======================================== */
+
+skinViewer.zoom = 0.8;
