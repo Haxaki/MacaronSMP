240b751ae3b65920f44c3ea4c7750be26220cabc
Haxaki_Hamane <nguyenngocdanthanh7a2@gmail.com>
2026-09-16 13:41:04 +0700

    Update Mako.js

--- Mako.js
+++ Mako.js
@@ -2,10 +2,6 @@
    MACARON SMP — MAKO PROFILE
 ======================================== */
 
-
-/* ========================================
-   CREATE SKIN VIEWER
-======================================== */
 const canvas = document.getElementById("skin_container");
 
 console.log("skinview3d:", typeof skinview3d);
@@ -18,51 +14,21 @@ const skinViewer = new skinview3d.SkinViewer({
     skin: "./Mako.png"
 });
 
-skinViewer.autoRotate = true;
-skinViewer.autoRotateSpeed = 0.5;
-skinViewer.zoom = 0.8;
-
 /* ========================================
    SKIN POSITION
 ======================================== */
 
 skinViewer.playerObject.position.y = -0.5;
 
-
 /* ========================================
    AUTO ROTATION
 ======================================== */
 
 skinViewer.autoRotate = true;
-
 skinViewer.autoRotateSpeed = 0.5;
-
 
 /* ========================================
    ZOOM
 ======================================== */
 
 skinViewer.zoom = 0.8;
-
-
-/* ========================================
-   MOUSE CONTROLS
-======================================== */
-
-const control =
-    skinview3d.createOrbitControls(skinViewer);
-
-
-/* Drag mouse */
-
-control.enableRotate = true;
-
-
-/* Scroll wheel */
-
-control.enableZoom = true;
-
-
-/* Disable moving the model */
-
-control.enablePan = false;
