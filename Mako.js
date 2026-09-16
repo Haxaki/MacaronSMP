f1c1063b255a4278e5d07db8100b0d98541923d7
Haxaki_Hamane <nguyenngocdanthanh7a2@gmail.com>
2026-09-16 13:21:11 +0700

    Add files via upload

---
+++ Mako.js
@@ -0,0 +1,68 @@
+/* ========================================
+   MACARON SMP — FRENCH PROFILE
+======================================== */
+
+
+/* ========================================
+   CREATE SKIN VIEWER
+======================================== */
+const canvas = document.getElementById("skin_container");
+
+console.log("skinview3d:", typeof skinview3d);
+console.log("canvas:", canvas);
+
+const skinViewer = new skinview3d.SkinViewer({
+    canvas: canvas,
+    width: 400,
+    height: 500,
+    skin: "./Mako.PNG"
+});
+
+skinViewer.autoRotate = true;
+skinViewer.autoRotateSpeed = 0.5;
+skinViewer.zoom = 0.8;
+
+/* ========================================
+   SKIN POSITION
+======================================== */
+
+skinViewer.playerObject.position.y = -0.5;
+
+
+/* ========================================
+   AUTO ROTATION
+======================================== */
+
+skinViewer.autoRotate = true;
+
+skinViewer.autoRotateSpeed = 0.5;
+
+
+/* ========================================
+   ZOOM
+======================================== */
+
+skinViewer.zoom = 0.8;
+
+
+/* ========================================
+   MOUSE CONTROLS
+======================================== */
+
+const control =
+    skinview3d.createOrbitControls(skinViewer);
+
+
+/* Drag mouse */
+
+control.enableRotate = true;
+
+
+/* Scroll wheel */
+
+control.enableZoom = true;
+
+
+/* Disable moving the model */
+
+control.enablePan = false;
