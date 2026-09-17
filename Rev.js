28878b62ad99f4c2ca7bdf18404f2169bec79310
Haxaki_Hamane <nguyenngocdanthanh7a2@gmail.com>
2026-09-17 14:05:26 +0700

    Add files via upload

---
+++ Rev.js
@@ -0,0 +1,34 @@
+/* ========================================
+   MACARON SMP — Macaron PROFILE
+======================================== */
+
+const canvas = document.getElementById("skin_container");
+
+console.log("skinview3d:", typeof skinview3d);
+console.log("canvas:", canvas);
+
+const skinViewer = new skinview3d.SkinViewer({
+    canvas: canvas,
+    width: 400,
+    height: 500,
+    skin: "./Rae.png"
+});
+
+/* ========================================
+   SKIN POSITION
+======================================== */
+
+skinViewer.playerObject.position.y = -0.5;
+
+/* ========================================
+   AUTO ROTATION
+======================================== */
+
+skinViewer.autoRotate = true;
+skinViewer.autoRotateSpeed = 0.5;
+
+/* ========================================
+   ZOOM
+======================================== */
+
+skinViewer.zoom = 0.8;
