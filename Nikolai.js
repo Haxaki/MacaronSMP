4a7514f796eb1b6493d1871cef978bc14d263398
Haxaki_Hamane <nguyenngocdanthanh7a2@gmail.com>
2026-09-21 22:04:37 +0700

    Add files via upload

---
+++ Nikolai.js
@@ -0,0 +1,34 @@
+/* ========================================
+   MACARON SMP — Nikolai PROFILE
+======================================== */
+
+const canvas = document.getElementById("skin_container");
+
+console.log("skinview3d:", typeof skinview3d);
+console.log("canvas:", canvas);
+
+const skinViewer = new skinview3d.SkinViewer({
+    canvas: canvas,
+    width: 400,
+    height: 500,
+    skin: "./.png"
+});
+
+/* ========================================
+   SKIN POSITION
+======================================== */
+
+skinViewer.playerObject.position.y = -0.5;
+
+/* ========================================
+   AUTO ROTATION
+======================================== */
+
+skinViewer.autoRotate = true;
+skinViewer.autoRotateSpeed = 0.5;
+
+/* ========================================
+   ZOOM
+======================================== */
+
+skinViewer.zoom = 0.8;
