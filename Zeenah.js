fa2606a815c5fd2f3d065e762e795dbbe5102d5d
Haxaki_Hamane <nguyenngocdanthanh7a2@gmail.com>
2026-09-21 21:32:04 +0700

    Update Zeenah.js

--- Zeenah.js
+++ Zeenah.js
@@ -1,5 +1,5 @@
 /* ========================================
-   MACARON SMP — AtelierIndustry PROFILE
+   MACARON SMP — ZeeliZee PROFILE
 ======================================== */
 
 const canvas = document.getElementById("skin_container");
@@ -11,7 +11,7 @@ const skinViewer = new skinview3d.SkinViewer({
     canvas: canvas,
     width: 400,
     height: 500,
-    skin: "./Jena.png"
+    skin: "./Zee.png"
 });
 
 /* ========================================
